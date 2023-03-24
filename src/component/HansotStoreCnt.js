c5868bb3ebc2a9890f35bc9cc2d37d4b3ef7fada
yeonsukkk <dustn910720@gmail.com>
2023-03-24 16:59:09 +0900

    가맹점찾기 페이지, 지도 넣기

--- src/component/HansotStoreCnt.js
+++ src/component/HansotStoreCnt.js
@@ -1,3 +1,4 @@
+import Iframe from 'react-iframe'
 import './HansotStoreCnt.css'
 
 const HansotStoreCnt = (props) => {
@@ -16,7 +17,7 @@ const HansotStoreCnt = (props) => {
   return(
     <div className="HansotStoreWrap">
       <div className="map">
-
+        <Iframe url={props.map} />
       </div>
       <ul className="storeList">
         {
@@ -26,7 +27,11 @@ const HansotStoreCnt = (props) => {
                 <dt>{item.title}</dt>
                 <dd>{item.tel}</dd>
                 <dd>{item.address}</dd>
-                <dd><button type="button">지도보기</button></dd>
+                <dd>
+                  <button type="button"
+                    onClick={() => props.setMap(item.map)}
+                  >지도보기</button>
+                </dd>
               </dl>
             </li>
           ))
--- src/page/HansotStore.js
+++ src/page/HansotStore.js
@@ -14,7 +14,9 @@ const HansotStore = () => {
         fetchData()
     }, [])
 
-    const [opt, setOpt] = useState('전체')
+    const [opt, setOpt] = useState('전체') // select
+    const [map, setMap] = useState('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3166.6359537339267!2d126.93770110000001!3d37.4693165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9fe8a0a1e2a5%3A0xccc97446c4ec08bd!2z7ZWc7Ial64-E7Iuc6529IOyLoOumvOqzoOyLnOy0jOygkA!5e0!3m2!1sko!2skr!4v1679641975919!5m2!1sko!2skr')
+    
 
     //필터
     let filterItem = store.data?.filter(item => {return opt !== '전체' ? item.title === opt : store.data})
@@ -25,8 +27,8 @@ const HansotStore = () => {
             <div className="comWebWidth">
                 <h3 className="mainTitle">가맹점찾기</h3>
                 <div className="cntWrap">
-                    <HansotStoreOpt data={store.data} getOpt={(getOpt) => setOpt(getOpt)} />
-                    <HansotStoreCnt data={filterItem} opt={opt} />
+                    {<HansotStoreOpt data={store.data} getOpt={(getOpt) => setOpt(getOpt)} />}
+                    {<HansotStoreCnt data={filterItem} opt={opt} map={map} setMap={setMap} />}
                 </div>
                 <Event />
             </div>
