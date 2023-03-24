29ab9b7746070168d1e6fda9507051412295800c
yeonsukkk <dustn910720@gmail.com>
2023-03-24 13:27:31 +0900

    뉴스 페이지 추가

--- src/page/News.js
+++ src/page/News.js
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import NewsItem from "../component/NewsItem";
+
+import './News.css'
+
 const News = () => {
+    const [news, setNews] = useState([]) // data담기
+    useEffect(() => { // data가져오기
+        const fetchData = async() => {
+            const response = await axios.get('../json/newsData.json')
+            setNews(response)
+        }
+        fetchData()
+    },[])
+
+    console.log(news.data)
     return(
         <>
-            News
+        <section className="newsWrap subWrap">
+            <div className="comWebWidth">
+                <h3 className="mainTitle">새로운 소식</h3>
+                <div className="cntWrap">
+                    <NewsItem data={news.data} />
+                </div>
+            </div>
+        </section>
         </>
     )
 }
